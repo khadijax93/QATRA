167c1069c586fe429de109f2a2ecc6a2ea748c28
khadijax93 <mbarkioukhadija@gmail.com>
2026-01-31 00:51:53 +0100

    Update schema.sql
    Add users table

--- database/schema.sql
+++ database/schema.sql
@@ -2,3 +2,15 @@
 -- Database Schema
 -- Oracle SQL Compatible
 -- Tables will be defined below
+-- ============================
+-- USERS TABLE
+-- ============================
+
+CREATE TABLE users (
+    user_id NUMBER PRIMARY KEY,
+    name VARCHAR2(100) NOT NULL,
+    email VARCHAR2(100) UNIQUE NOT NULL,
+    city VARCHAR2(50) NOT NULL,
+    role VARCHAR2(20)
+        CHECK (role IN ('HOSPITAL', 'DONOR', 'ADMIN'))
+);
