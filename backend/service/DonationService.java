a2b8dddc693afe67831e2eb4ff7152939ef8d334
khadijax93 <mbarkioukhadija@gmail.com>
2026-01-31 16:15:15 +0100

    Update DonationService.java

--- backend/service/DonationService.java
+++ backend/service/DonationService.java
@@ -1,1 +1,16 @@
+package service;
+
+import model.Donor;
+
+public class DonationService {
+
+    public void processDonation(Donor donor) {
+        if (donor.isEligible()) {
+            donor.donate();
+            System.out.println(donor.getName() + " donated successfully.");
+        } else {
+            System.out.println("Donor is not eligible to donate.");
+        }
+    }
+}
 
