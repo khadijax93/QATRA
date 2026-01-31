3f0abd79758f41d7fadbb08f7dee50d241521981
khadijax93 <mbarkioukhadija@gmail.com>
2026-01-31 16:27:45 +0100

    Update main.java

--- backend/main.java
+++ backend/main.java
@@ -1,1 +1,21 @@
+import model.Donor;
+import service.DonationService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create a donor
+        Donor donor = new Donor("Khadija", "O+");
+
+        // Create the service
+        DonationService service = new DonationService();
+
+        // First donation (should succeed)
+        service.processDonation(donor);
+
+        // Second donation (should fail)
+        service.processDonation(donor);
+    }
+}
 
