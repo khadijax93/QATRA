f741a858ee1a96c83a51e48a8be1076654d2b7a2
khadijax93 <mbarkioukhadija@gmail.com>
2026-01-31 15:52:52 +0100

    Update Donor.java

--- backend/model/Donor.java
+++ backend/model/Donor.java
@@ -1,1 +1,27 @@
+package model;
+
+public class Donor {
+
+    private String name;
+    private String bloodType;
+    private boolean eligible;
+
+    public Donor(String name, String bloodType) {
+        this.name = name;
+        this.bloodType = bloodType;
+        this.eligible = true;
+    }
+
+    public boolean isEligible() {
+        return eligible;
+    }
+
+    public void donate() {
+        this.eligible = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
